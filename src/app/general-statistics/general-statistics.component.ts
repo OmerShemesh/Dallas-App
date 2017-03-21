3d6a69195e2446de1046b1d20a5fdc0f0ac7d778
Omer Shemsheyan <omershemesh95@gmail.com>
2017-03-21 21:39:52 +0200

    added tabs and vm os types

--- src/app/general-statistics/general-statistics.component.ts
+++ src/app/general-statistics/general-statistics.component.ts
@@ -14,17 +14,20 @@ export class GeneralStatisticsComponent implements OnInit {
   labels = [];
   data = [];
   ready: boolean = false;
-  chartOptions =  {
-          tooltips: {
-            mode: 'label',
-            callbacks: {
-              label: function (tooltipItem, data) {
-                var tooltipLabel = data.labels[tooltipItem.index];
+  chartOptions = {
+    legend: {
+      display: true
+    },
+    tooltips: {
+      mode: 'label',
+      callbacks: {
+        label: function (tooltipItem, data) {
+          var tooltipLabel = data.labels[tooltipItem.index];
 
-                return tooltipLabel + ': ' + data['datasets'][0]['data'][tooltipItem['index']] + '%';
-              }
-            }
-          }
+          return tooltipLabel + ': ' + data['datasets'][0]['data'][tooltipItem['index']] + '%';
+        }
+      }
+    }
   };
 
 
@@ -41,43 +44,56 @@ export class GeneralStatisticsComponent implements OnInit {
 
   constructor(private route: ActivatedRoute, private statisticsService: StatisticsService) { }
 
-  onOptionChange() {
-      this.ready = false;
-      this.data = [];
-      this.labels = [];
-      this.statisticsService.getGeneralStatistics(this.selectedOption).subscribe(statistics => {
-       
-        this.statistics = statistics;
 
-        if(this.selectedOption == 'clusters'){
-          let keys = this.labels = Object.keys(statistics.ovirt_versions);
-          this.labels = this.labels.map(label => "oVirt " + label);
-          for(let key of keys){
-            this.data.push(statistics.ovirt_versions[key]);
-          }
-          
+
+  onSelect(index) {
+
+    this.selectedOption = this.options[index].value;
+    this.ready = false;
+    this.data = [];
+    this.labels = [];
+    this.statisticsService.getGeneralStatistics(this.selectedOption).subscribe(statistics => {
+
+      this.statistics = statistics;
+
+      if (this.selectedOption == 'clusters') {
+        this.chartOptions.legend.display = true;
+        let keys = this.labels = Object.keys(statistics.ovirt_versions);
+        this.labels = this.labels.map(label => "oVirt " + label);
+        for (let key of keys) {
+          this.data.push(statistics.ovirt_versions[key]);
         }
 
-        else if(this.selectedOption == 'storage'){
-          let keys = this.labels = Object.keys(statistics.storage_types);
-          for(let key of keys){
-            this.data.push(statistics.storage_types[key]);
-          }
-          
+      }
+
+      else if (this.selectedOption == 'storage') {
+        this.chartOptions.legend.display = true;
+        let keys = this.labels = Object.keys(statistics.storage_types);
+        for (let key of keys) {
+          this.data.push(statistics.storage_types[key]);
         }
 
-        else if(this.selectedOption == 'hosts'){
-           let keys = this.labels = Object.keys(statistics.cpus);
-          for(let key of keys){
-            this.data.push(statistics.cpus[key]);
-          }      
+      }
+
+      else if (this.selectedOption == 'hosts') {
+        this.chartOptions.legend.display = true;
+        let keys = this.labels = Object.keys(statistics.cpus);
+        for (let key of keys) {
+          this.data.push(statistics.cpus[key]);
         }
+      }
 
-        this.ready = true;
-      });
+      else if (this.selectedOption == 'vms') {
+        this.chartOptions.legend.display = false;
+        let keys = this.labels = Object.keys(statistics.os_types);
+        for (let key of keys) {
+          this.data.push(statistics.os_types[key]);
+        }
+      }
 
+      this.ready = true;
+    });
 
-    
   }
 
   ngOnInit() {
